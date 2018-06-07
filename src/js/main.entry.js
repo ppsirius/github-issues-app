b9bb7b871b49ed7e449288dc7e0498aa9a0134f4
Piotr Pupczyk <p.pupczyk@samsung.com>
2018-06-07 14:24:46 +0200

    add scrollbar

--- src/js/main.entry.js
+++ src/js/main.entry.js
@@ -1,4 +1,24 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('fdsffssfd');
+import PerfectScrollbar from "perfect-scrollbar";
 
+window.addEventListener("load", () => {
+  appInit();
+});
+
+function appInit() {
+  // Scrollbar
+  new PerfectScrollbar(".issues");
+
+  // Mark Icons Toggle
+  const markIcons = document.querySelectorAll(".issue-mark-icon");
+  markIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+      toogleMarkIcon(icon);
+    });
+  });
+}
+
+function toogleMarkIcon(element) {
+  element.classList.toggle("active");
+}
