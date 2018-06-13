444d4f875e34413a6a2d9b7ec3dbb6624b224337
Piotr Pupczyk <p.pupczyk@samsung.com>
2018-06-13 14:50:15 +0200

    icon toggle to checkbox

--- src/js/main.entry.js
+++ src/js/main.entry.js
@@ -9,16 +9,4 @@ window.addEventListener("load", () => {
 function appInit() {
   // Scrollbar
   new PerfectScrollbar(".issues");
-
-  // Mark Icons Toggle
-  const markIcons = document.querySelectorAll(".issue-mark-icon");
-  markIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-      toogleMarkIcon(icon);
-    });
-  });
-}
-
-function toogleMarkIcon(element) {
-  element.classList.toggle("active");
 }
